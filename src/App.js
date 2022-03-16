5904fb42a5af44915f091847e9af88f040ea9898
Shushan <shushogrig91@gmail.com>
2022-03-16 15:31:02 +0400

    shushan change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import{BrowserRouter,route} from "react-router-dom"
+import {Slider} from "./components/slider"
+import Calendar from 'react-calendar';
+import {Event} from "./components/event"
+import 'react-calendar/dist/Calendar.css';
 import Categoreis from './components/header/links';
 
 
 function App() {
+  const [value, onChange] = useState(new Date());
+
   return ( 
   <div>
   <BrowserRouter>
     <Header /> 
   
+    <Slider/>
+    <Calendar onChange={onChange} value={value} />
+    <Event/>
     
     </BrowserRouter>
 
